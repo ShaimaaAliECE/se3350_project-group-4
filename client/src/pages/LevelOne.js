d10e3738a0e40ba3645a38b8268e7ac423d27f43
Saummmm <sbuch2@uwo.ca>
2022-01-27 17:41:19 -0500

    working on level 1

--- client/src/pages/LevelOne.js
+++ client/src/pages/LevelOne.js
@@ -1,12 +1,41 @@
 import React from "react";
 
 class LevelOne extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      finishedPlaying: false,
+      paused: true,
+    };
+  }
   render() {
     return (
-      //header
       <div className="">
-        <div></div>
+        <div className="header"></div>
+        <div className="body">
+          <div className="blocks"></div>
+          <div className="alg-steps"></div>
+          <div className="play/pause/playagain">
+            {this.state.finishedPlaying} ? {this.state.paused} ? <PlayButton />{" "}
+            : <PauseButton /> : <ReplayButton />
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+function PlayButton() {
+  return <div>play</div>;
+}
+
+function PauseButton() {
+  return <div>pause</div>;
+}
+
+function ReplayButton() {
+  return <div>Replay</div>;
+}
+
+export default LevelOne;
