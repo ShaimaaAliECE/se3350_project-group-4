825ee7d1bfc36cdb7267f3db2849af9c4b7058da
zhenxiao-yu <markyu0615@gmail.com>
2022-01-27 18:26:29 -0500

    Added component template for pause / gameover

---
+++ client/src/components/Pause.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Pause extends React.Component {
+  render() {
+    return (
+        <div className="menu-content">
+            {/* header */}
+            <p className="title has-text-centered">Paused</p>
+            {/* buttons */}
+            <div>
+            </div>
+        </div>
+    )
+  }
+}
+
+export default Pause;
