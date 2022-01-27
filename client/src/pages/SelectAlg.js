789eddcefe8b2b921795518850483230e3d7aaf0
zhenxiao-yu <markyu0615@gmail.com>
2022-01-26 20:40:00 -0500

    mark - updated select algorithm page UI

--- client/src/pages/SelectAlg.js
+++ client/src/pages/SelectAlg.js
@@ -1,34 +1,57 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
 class SelectAlg extends React.Component {
   render() {
     return (
       <div className="alg-wrapper">
-        <div className="alg-title">
-          Hello
-          <span> username</span>, Choose an algorithm to begin...
-        </div>
-
-        <div class="dropdown is-hoverable">
-          <div class="dropdown-trigger">
-            <button
-              class="button"
-              aria-haspopup="true"
-              aria-controls="dropdown-menu4"
-            >
-              <span>Hover me</span>
-              <span class="icon is-small">
-                <i class="fas fa-angle-down" aria-hidden="true"></i>
-              </span>
-            </button>
+        <div className="section">
+          {/*  */}
+          <div className="title label has-text-light">
+            <span>Hello </span>
+            <span className="has-text-primary">username</span>,
+            <div>Choose an algorithm to begin...</div>
           </div>
-          <div class="dropdown-menu" id="dropdown-menu4" role="menu">
-            <div class="dropdown-content">
-              <div class="dropdown-item">
-                <p>
-                  You can insert <strong>any type of content</strong> within the
-                  dropdown menu.
-                </p>
+          {/* dropdown */}
+          <div class="dropdown is-hoverable">
+            {/* dropdown box */}
+            <div class="dropdown-trigger">
+              {/* dropdown button */}
+              <button
+                className="button is-large  is-rounded has-background-primary"
+                aria-haspopup="true"
+                aria-controls="dropdown-menu"
+              >
+                {/* text */}
+                <span className="has-text-weight-semibold">
+                  Select from dropdown
+                </span>
+                {/* arrow icon */}
+                <span className="icon is-small is-justify-content-flex-end">
+                  <i className="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+              </button>
+            </div>
+            {/* dropdown menu */}
+            <div className="dropdown-menu" id="dropdown-menu" role="menu">
+              <div className="dropdown-content">
+                <Link to="/select">
+                  <div className="dropdown-item">
+                    <span className="dropdown-text">Merge Sort</span>
+                  </div>
+                </Link>
+                <hr class="dropdown-divider"></hr>
+                <Link to="/login">
+                  <div className="dropdown-item">
+                    <span className="dropdown-text">Merge Sort</span>
+                  </div>
+                </Link>
+                <hr class="dropdown-divider"></hr>
+                <Link to="/login">
+                  <div className="dropdown-item">
+                    <span className="dropdown-text">Merge Sort</span>
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
@@ -38,4 +61,4 @@ class SelectAlg extends React.Component {
   }
 }
 
-export default SelectAlg;
+export default withRouter(SelectAlg);
