6411f9ad3b6ba3e48490ace478489caece7bfcd6
zhenxiao-yu <markyu0615@gmail.com>
2022-01-28 15:57:52 -0500

    mark - added form validation for login page

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import SelectAlg from "./pages/SelectAlg";
-import SelectLevel from "./pages/SelectLevel";
-import EditLevel from "./pages/Admin/EditLevel";
-import NotFound from "./pages/NotFound";
-import LevelOne from "./pages/Users/Levels/LevelOne";
-import LevelTwo from "./pages/Users/Levels/LevelTwo";
-import LevelThree from "./pages/Users/Levels/LevelThree";
-import LevelFour from "./pages/Users/Levels/LevelFour";
-import LevelFive from "./pages/Users/Levels/LevelFive";
-import CustomLevel from "./pages/Users/Levels/CustomLevel";
+import Home from "pages/Home";
+import Login from "pages/Login";
+import Register from "pages/Register";
+import SelectAlg from "pages/SelectAlg";
+import SelectLevel from "pages/SelectLevel";
+import EditLevel from "pages/Admin/EditLevel";
+import NotFound from "pages/NotFound";
+import LevelOne from "pages/Users/Levels/LevelOne";
+import LevelTwo from "pages/Users/Levels/LevelTwo";
+import LevelThree from "pages/Users/Levels/LevelThree";
+import LevelFour from "pages/Users/Levels/LevelFour";
+import LevelFive from "pages/Users/Levels/LevelFive";
+import CustomLevel from "pages/Users/Levels/CustomLevel";
 
 
 //router component for switching between pages
