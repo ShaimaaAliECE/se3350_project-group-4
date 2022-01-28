83cf8da63139d7973227c24b86a538c52144d898
zhenxiao-yu <markyu0615@gmail.com>
2022-01-27 22:27:22 -0500

    added interface and implemented function for user authentication

--- server/server.js
+++ server/server.js
@@ -1,18 +1,51 @@
 // json server
 const path = require("path");
+const fs = require("fs");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middleWares = jsonServer.defaults();
 
-//middleware
+//------middlewares----//
 server.use(jsonServer.bodyParser);
 server.use(middleWares);
 
-//define api
-server.get("/auth/login", (req, res) => {
-  console.log("Login Success");
-  return res.status(200).json("Login Success");
+//------functions-----//
+
+//fetch data from users.json
+const getUsersDb = () => {
+  return JSON.parse(
+    fs.readFileSync(path.join(__direname, "user.json"), "UTF-8")
+  );
+};
+
+//return user authentication result as boolean
+const isAuthenticated = ({ username, password }) => {
+  return (
+    getUsersDb().user.findIndex(
+      // check if user exists (verify user)
+      (user) => user.username === username && user.password === password
+    ) !== -1
+  ); //not equals to -1 means that user exists
+};
+
+//------apis---------//
+
+// login interface
+server.post("/auth/login", (req, res) => {
+  // get username and password from request
+  const { username, password } = req.body;
+
+  // return corresponding json based on user authentication result
+  if (isAuthenticated({})) {
+    const jsonWebToken = "xxxxxx.xxxxxx.xxxxxx"; //jwt authentication
+    return res.status(200).json(jsonWebToken);
+  } else {
+    //user authentication failed
+    const status = 401;
+    const msg = "Wrong username or password";
+    return res.status(status).json({ status, msg });
+  }
 });
 
 server.use(router);
