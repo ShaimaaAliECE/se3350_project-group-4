4cf24bc9260b9b0355df8413ee5625bf0e01ff2a
Sanjit Sharma <30699093+beingsanjit@users.noreply.github.com>
2022-01-31 10:41:14 -0500

    Added input boxes, event handlers
    Not yet fully mapping to correct target, but logic is intact

--- client/src/pages/Users/Levels/CustomLevel.js
+++ client/src/pages/Users/Levels/CustomLevel.js
@@ -1,17 +1,79 @@
 import React from "react";
 import LevelHeader from "../../../components/LevelHeader";
+import {
+    useState
+} from "react";
+import ReactDOM from "react-dom";
 
 class CustomLevel extends React.Component {
-  render() {
-    return 
-     //header
-      <div className="">
-        {/* header */}
-        <LevelHeader />
-      </div>
-    
-  }
-  
-}
+        render() {
+                function MyForm() {
+                    const [inputs, setInputs] = useState({});
 
-export default CustomLevel;
+                    const handleChange = (event) => {
+                        const name = event.target.name;
+                        const value = event.target.value;
+                        setInputs(values => ({
+                            ...values,
+                            [name]: value
+                        }))
+                    }
+
+                    const handleSubmit = (event) => {
+                        event.preventDefault();
+                        alert(inputs);
+                    }
+                    return (
+
+                        <
+                        form onSubmit = {
+                            handleSubmit
+                        } >
+                        <
+                        label > numberOfBoxes:
+                        <
+                        input type = "number"
+                        name = "numberOfBoxes"
+                        value = {
+                            inputs.numberOfBoxes || ""
+                        }
+                        onChange = {
+                            handleChange
+                        }
+                        /> <
+                        /label> <
+                        label > lowerRange:
+                        <
+                        input type = "number"
+                        name = "lowerRange"
+                        value = {
+                            inputs.lowerRange || ""
+                        }
+                        onChange = {
+                            handleChange
+                        }
+                        /> 
+                          <
+                        label > upperRange:
+                        <
+                        input type = "number"
+                        name = "upperRange"
+                        value = {
+                            inputs.upperRange || ""
+                        }
+                        onChange = {
+                            handleChange
+                        }
+                        />
+                          <
+                        /label> <
+                        input type = "submit" / >
+                        <
+                        /form>
+
+  
+                    }
+                    ReactDOM.render( < MyForm / > , document.getElementById('root'));
+                }
+
+                export default CustomLevel;
