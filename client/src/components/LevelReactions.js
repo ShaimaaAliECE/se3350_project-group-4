2fbcb1fd866e437d38f4c6caddac92363f416b63
Aaron-Moses <71098038+Aaron-Moses@users.noreply.github.com>
2022-02-01 12:48:26 -0500

    experimental sound
    commented

--- client/src/components/LevelReactions.js
+++ client/src/components/LevelReactions.js
@@ -2,8 +2,12 @@ import React from "react";
 
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+//import { Player } from "tone";
 toast.configure()
 
+
+//const RightSound = new Audio('client\src\audioclips\RightSound.mp3');
+//const WrongSound = new Audio('client\src\audioclips\WrongSound.mp3');
 
 function PositiveNotification(){
     const notify = ()=>{
@@ -11,6 +15,7 @@ function PositiveNotification(){
             position: toast.POSITION.BOTTOM_RIGHT, autoClose:2000})
           
     }
+    //Play.RightSound();
     /*
     return (
         <div className="NegativeNotification">
@@ -24,6 +29,7 @@ function NegativeNotification(){
         toast.error('Runtime error', {
             position: toast.POSITION.BOTTOM_RIGHT, autoClose:2000})          
     }
+    //play.WrongSound();
     /*
     return (
         <div className="NegativeNotification">
