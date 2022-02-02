b0fa8470cf8ac86fd54da9b41d64dd0e0d516e67
Saummmm <sbuch2@uwo.ca>
2022-02-02 17:00:37 -0500

    added blocks and sequence to level one and merge sort

--- client/src/pages/LevelOne.js
+++ client/src/pages/LevelOne.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LevelHeader from "../components/LevelHeader";
+import MergeSort from "../algorithms/mergeSort.mjs";
 
 class LevelOne extends React.Component {
   constructor(props) {
@@ -25,6 +26,45 @@ class LevelOne extends React.Component {
   }
 
   render() {
+    let split = [];
+    let merge = [];
+    var sorting = new MergeSort(1, 20, 10);
+    let sorted = sorting.sort(sorting.getArray(), split, merge);
+
+    let storageSplit = [];
+    let storageMerge = [];
+    let allElements = [];
+
+    let temp = [];
+
+    for (let i = 0; i < split.length; i++) {
+      if (!temp.includes(split[i].charAt(0))) {
+        storageSplit.push([[split[i].slice(2)]]);
+        temp.push(split[i].charAt(0));
+      } else {
+        storageSplit[split[i].charAt(0)].push([split[i].slice(2)]);
+      }
+    }
+
+    for (let i = 0; i < storageSplit[0][0][0].split(",").length; i++) {
+      // console.log(storageSplit[0][0][0].split(","));
+      allElements.push(storageSplit[0][0][0].split(",")[i]);
+    }
+
+    let cap = storageSplit.length - 1;
+    temp = [];
+
+    for (let i = 0; i < merge.length; i++) {
+      let index = cap - merge[i].charAt(0);
+      let data = merge[i].slice(2);
+
+      if (!temp.includes(index)) {
+        storageMerge.push([[data]]);
+        temp.push(index);
+      } else {
+        storageMerge[index].push([data]);
+      }
+    }
     let paused = this.state.paused;
     let button;
     if (paused) {
@@ -34,10 +74,57 @@ class LevelOne extends React.Component {
     }
     return (
       <div className="">
-        <div className="header"></div>
+        <div className="header">
+          <LevelHeader />
+        </div>
         <div className="body">
-          <div className="blocks"></div>
-          <div className="alg-steps"></div>
+          <div className="sort"></div>
+          <div className="alg-steps">
+            <div>
+              <div className="tile is-ancestor is-vertical">
+                {storageSplit.map((element) => {
+                  return (
+                    <div className="tile is-parent mt-6 mx-6">
+                      {element.map((innerElement) => {
+                        return (
+                          <div className="tile is-child has-text-centered box mx-5">
+                            {innerElement}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="tile is-ancestor is-vertical">
+                <div className="tile is-parent mt-6 mx-6">
+                  {allElements.map((element) => {
+                    return (
+                      <div className="tile is-child has-text-centered box mx-5">
+                        <div id="">{element}</div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="tile is-ancestor is-vertical">
+                {storageMerge.map((element) => {
+                  return (
+                    <div className="tile is-parent mt-6 mx-6">
+                      {element.map((innerElement) => {
+                        return (
+                          <div className="tile is-child has-text-centered box mx-5">
+                            {innerElement}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            );
+          </div>
           <div className="play/pause/playagain">{button}</div>
         </div>
       </div>
