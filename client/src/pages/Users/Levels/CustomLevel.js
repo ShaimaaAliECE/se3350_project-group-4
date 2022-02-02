4d9d16ac6aa4be8c6e968b169d2d71900e90dd28
Sanjit Sharma <30699093+beingsanjit@users.noreply.github.com>
2022-02-02 16:55:41 -0500

    Update CustomLevel.js

--- client/src/pages/Users/Levels/CustomLevel.js
+++ client/src/pages/Users/Levels/CustomLevel.js
@@ -1,79 +1,10 @@
 import React from "react";
-import LevelHeader from "../../../components/LevelHeader";
-import {
-    useState
-} from "react";
-import ReactDOM from "react-dom";
 
-// class CustomLevel extends React.Component {
-//         render() {
-//                 function MyForm() {
-//                     const [inputs, setInputs] = useState({});
-
-                    const handleChange = (event) => {
-                        const name = event.target.name;
-                        const value = event.target.value;
-                        setInputs(values => ({
-                            ...values,
-                            [name]: value
-                        }))
-                    }
-
-                    const handleSubmit = (event) => {
-                        event.preventDefault();
-                        alert(inputs);
-                    }
-                    return (
-
-                        <
-                        form onSubmit = {
-                            handleSubmit
-                        } >
-                        <
-                        label > numberOfBoxes:
-                        <
-                        input type = "number"
-                        name = "numberOfBoxes"
-                        value = {
-                            inputs.numberOfBoxes || ""
-                        }
-                        onChange = {
-                            handleChange
-                        }
-                        /> <
-                        /label> <
-                        label > lowerRange:
-                        <
-                        input type = "number"
-                        name = "lowerRange"
-                        value = {
-                            inputs.lowerRange || ""
-                        }
-                        onChange = {
-                            handleChange
-                        }
-                        /> 
-                          <
-                        label > upperRange:
-                        <
-                        input type = "number"
-                        name = "upperRange"
-                        value = {
-                            inputs.upperRange || ""
-                        }
-                        onChange = {
-                            handleChange
-                        }
-                        />
-                          <
-                        /label> <
-                        input type = "submit" / >
-                        <
-                        /form>
-
+class CustomLevel extends React.Component {
+  render() {
+    return <div> Custom Level Page </div>;
+  }
   
-                    }
-                    ReactDOM.render( < MyForm / > , document.getElementById('root'));
-                }
+}
 
-                export default CustomLevel;
+export default CustomLevel;
