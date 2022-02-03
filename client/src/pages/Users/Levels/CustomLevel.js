b58f6cbcef73fbd3303a6afd54f6c75d4ed11d54
RajPatel <rpate366@uwo.ca>
2022-02-02 19:35:30 -0500

    Finished the Setup for the CustomLevel

--- client/src/pages/Users/Levels/CustomLevel.js
+++ client/src/pages/Users/Levels/CustomLevel.js
@@ -1,29 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import Modal from 'react-modal';
 import LevelHeader from "../../../components/LevelHeader";
 
-
-class Level extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    
-    render() {
-        return (
-            <div>
-                <div className="">
-                    {/* header */}
-                    <LevelHeader />
-                </div>
-                <div>
-                    <label>
-                        {/* {this.props.boxes} */}
-                    </label>
-                </div>
-            </div>
-        )
-    }
-}
 
 class CustomLevel extends React.Component {
     constructor(props) {
@@ -31,63 +9,73 @@ class CustomLevel extends React.Component {
         this.state = {
             numOfBoxes: 15,
             upperLimit: 50,
-            lowerLimit: 0
+            lowerLimit: 0,
+            showModal: true
         }
+        this.handleOpenModal = this.handleOpenModal.bind(this);
     }
 
-    handleBoxes(event) {
-        this.setState({numOfBoxes: event.target.value})
+    handleOpenModal () {
+        this.setState({ showModal: true });
     }
 
-    handleUpperLimit(event) {
-        this.setState({upperLimit: event.target.value})
+    startLevel = (event) => {
+        this.setState({
+            numOfBoxes: event.target[0].value,
+            upperLimit: event.target[1].value,
+            lowerLimit: event.target[2].value,
+            showModal: false})
+        this.setState()
     }
 
-    handleLowerLimit(event) {
-        this.setState({lowerLimit: event.target.value})
-    }
-
-    startLevel(){
-        ReactDOM.render(<Level 
-            boxes = {this.state.numOfBoxes} 
-            upLim = {this.state.upperLimit} 
-            lowLim = {this.state.lowerLimit}/>, 
-            document.getElementById('root'))
+    componentDidMount() {
+        this.handleOpenModal();
     }
 
     render() {
+        
         return(
             <div>
-                <div className="">
-                    {/* header */}
-                    <LevelHeader />
+                <div>
+                    <div className="">
+                        {/* header */}
+                        <LevelHeader />
+                    </div>
+                    <div className="level">
+                        {/* level */}
+                        {this.state.numOfBoxes}
+                        {this.state.upperLimit}
+                        {this.state.lowerLimit}
+                    </div>
                 </div>
-                <form onSubmit={this.startLevel}>
-                    <label>Number Of Boxes:
-                        <input 
-                        type="number"
-                        min="3"
-                        max="30"
-                        value={this.state.numOfBoxes}
-                        onChange={this.handleBoxes}
-                        />
-                    </label>
-                    <label>Upper Limit:
-                        <input
-                        type="number"
-                        value={this.state.upperLimit}
-                        onChange={this.handleUpperLimit}
-                        />
-                    </label>
-                    <label>Lower Limit:
-                        <input
-                        type="number"
-                        value={this.state.lowerLimit} 
-                        onChange={this.handleLowerLimit}
-                        />
-                    </label>
-                <input type="submit" />
-                </form>
+                <Modal isOpen={this.state.showModal} ariaHideApp = {false}>
+                    <form onSubmit={this.startLevel}>
+                                <label>Number Of Boxes:
+                                    <input 
+                                    type="number"
+                                    min="3"
+                                    max="30"
+                                    defaultValue={this.state.numOfBoxes}
+                                    name="nbox" 
+                                    />
+                                </label>
+                                <label>Upper Limit:
+                                    <input
+                                    type="number"
+                                    defaultValue={this.state.upperLimit}
+                                    name="ulim"
+                                    />
+                                </label>
+                                <label>Lower Limit:
+                                    <input
+                                    type="number"
+                                    defaultValue={this.state.lowerLimit} 
+                                    name="llim"
+                                    />
+                                </label>
+                                <input type="Submit" />
+                    </form>
+                </Modal>
             </div>  
         );
     
