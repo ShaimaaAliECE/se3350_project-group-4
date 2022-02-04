18c52856f043068a116f3c8617e36285db1c63bc
Aaron-Moses <71098038+Aaron-Moses@users.noreply.github.com>
2022-02-03 19:25:27 -0500

    WORKING AUDIO

--- client/src/components/LevelReactions.js
+++ client/src/components/LevelReactions.js
@@ -1,40 +1,48 @@
+import { Howl } from "howler";
 import React from "react";
 
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-//import { Player } from "tone";
+
+import RightSound from 'client/src/audioclips/RightSound.mp3';
+import WrongSound from 'client/src/audioclips/WrongSound.mp3';
+
 toast.configure()
 
+function Notification(){
 
-//const RightSound = new Audio('client\src\audioclips\RightSound.mp3');
-//const WrongSound = new Audio('client\src\audioclips\WrongSound.mp3');
-
-function PositiveNotification(){
-    const notify = ()=>{
-        toast.success('successful', {
-            position: toast.POSITION.BOTTOM_RIGHT, autoClose:2000})
-          
+    function Soundplayer(){
+      new Audio(RightSound).play();
     }
-    //Play.RightSound();
-    /*
-    return (
-        <div className="NegativeNotification">
-            <button onClick={notify}>Click Me!</button>
-            </div>
-    );*/
-}
-
-function NegativeNotification(){
-    const notify = ()=>{
-        toast.error('Runtime error', {
-            position: toast.POSITION.BOTTOM_RIGHT, autoClose:2000})          
+    function Soundplayer2(){
+      new Audio(WrongSound).play();
     }
-    //play.WrongSound();
-    /*
-    return (
-        <div className="NegativeNotification">
-            <button onClick={notify}>Click Me!</button>
-            </div>
-    );
-    */
-}
+  
+      function Correctnotify(){
+        toast.success('Correct!')
+    }
+      function Incorrectnotify(){
+        toast.error('Incorrect!')
+      }
+      
+  
+      return(
+        <div className="CORRECT">
+              <button onClick={() => {
+                Correctnotify();
+                Soundplayer();
+              }}>Click Me!</button>
+              <div className="INCORRECT">
+              <button onClick={() => {
+                Incorrectnotify();
+                Soundplayer2();
+              }}>Click Me!</button>
+              </div>  
+              </div>
+               );
+            
+  
+    };
+  
+    export default Notification;
+  
