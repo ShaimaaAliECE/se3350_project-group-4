4c755b12844f052ab0045f3e7e0e084803160093
zhenxiao-yu <markyu0615@gmail.com>
2022-02-10 15:17:51 -0500

    added generate array util class

---
+++ client/src/utils/generateArray.js
@@ -0,0 +1,15 @@
+// generate an random array of 
+
+//(1) size 'count' 
+//(2) a lower limit of 'min'
+//(3) an upper limit of 'max'
+
+export default function generateArray(count, min, max) {
+  // declare empty array
+  let result = [];
+  // add 'count' amount of numbers 
+  for (let i = 0; i < count; i++) {
+    result.push(Math.floor(Math.random() * (max + 1 - min) + min));
+  }
+  return result;
+}
