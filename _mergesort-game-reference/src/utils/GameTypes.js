0e9f0eabbc35e8a6f0c5db7341f8ab80d0f275c2
zhenxiao-yu <markyu0615@gmail.com>
2022-02-10 16:23:49 -0500

    Revert "added demo"
    This reverts commit 0d1c6e4115759a785ef937c7da35c625e6e8e0c2.

--- _mergesort-game-reference/src/utils/GameTypes.js
+++
@@ -1,9 +0,0 @@
-export var ArrayStates;
-(function (ArrayStates) {
-    ArrayStates["UNSORTED"] = "unsorted";
-    ArrayStates["LEFT_SORTING"] = "left_sorting";
-    ArrayStates["RIGHT_SORTING"] = "right_sorting";
-    ArrayStates["MERGING"] = "merging";
-    ArrayStates["MERGED"] = "merged";
-})(ArrayStates || (ArrayStates = {}));
-//# sourceMappingURL=GameTypes.js.map
--- _mergesort-game-reference/src/utils/GameTypes.ts
+++
@@ -1,7 +0,0 @@
-export enum ArrayStates {
-    UNSORTED = "unsorted",
-    LEFT_SORTING = "left_sorting",
-    RIGHT_SORTING = "right_sorting",
-    MERGING = "merging",
-    MERGED = "merged"
-}
