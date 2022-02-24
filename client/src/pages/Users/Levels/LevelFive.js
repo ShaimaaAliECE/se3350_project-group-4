a107f499300e9f4595a22bc27b10d426caa14eb6
Sanjit Sharma <30699093+beingsanjit@users.noreply.github.com>
2022-02-24 10:22:12 -0500

    Porting some basics from other levels

--- client/src/pages/Users/Levels/LevelFive.js
+++ client/src/pages/Users/Levels/LevelFive.js
@@ -1,15 +1,37 @@
 import React from "react";
-import LevelHeader from "components/LevelComponents/LevelHeader";
+import LevelHeader from "components/LevelHeader";
+import Modal from "components/Modal";
+import { toast } from "react-toastify";
+
+toast.configure();
 
 class LevelFive extends React.Component {
-  render() {
-    return (
-      <div className="">
-        {/* header */}
-        <LevelHeader level="5" />
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialArr: [],
+      splitting: true,
+      step: 0,
+      instructions: [],
+      order: [],
+      splitOrder: [],
+      //show modal when page loads
+      showModal: true, 
+      win: false,
+    };
+    this.generateArray = this.generateArray.bind(this);
+    this.handleNextStep = this.handleNextStep.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.handleMerge = this.handleMerge.bind(this);
+    this.handleStart = this.handleStart.bind(this);
+    this.handleEnd = this.handleEnd.bind(this);
+    this.checkCorrect = this.checkCorrect.bind(this);
+  }
+
+  // execute when start on the modal is pressed
+  handleStart() {
+    this.generateArray();
+    this.setState({ showModal: false });
   }
 }
-
 export default LevelFive;
