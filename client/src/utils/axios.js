1d8d0d454e14d928a347e36bcb68e49820e05f2d
zhenxiao-yu <markyu0615@gmail.com>
2022-03-03 15:03:56 -0500

    production

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,7 +4,7 @@ import _axios from "axios";
 const axios = (baseURL) => {
   const instance = _axios.create({
     baseURL: baseURL || process.env.REACT_APP_API_DOMAIN ||"http://localhost:3001",
-    timeout: 1000,
+    timeout: 1500,
   });
 
   return instance;
