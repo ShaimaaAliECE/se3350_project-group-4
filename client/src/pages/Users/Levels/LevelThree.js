a6a15c3e3c79b4121b6949c8ae77c4b303c06dda
Aaron-Moses <71098038+Aaron-Moses@users.noreply.github.com>
2022-03-03 13:15:17 -0500

    positive notifications level 3
    still duplicates

--- client/src/pages/Users/Levels/LevelThree.js
+++ client/src/pages/Users/Levels/LevelThree.js
@@ -309,6 +309,11 @@ function Arrays(props) {
     new Audio(WrongSound).play();
   }
 
+  function CorrectAnswer() {
+    new Audio(CorrectAnswer).play();
+  }
+
+
   function evaluateOtherSplit(condition) {
     // console.log("Evaluating: " + right + " to " + condition)
 
@@ -356,7 +361,13 @@ function Arrays(props) {
         console.log("bad");
         SoundError();
         toast.error("INCORRECT");
-      }
+        sorted = true;
+      } else if (sorted) {
+      console.log("nice");
+      toast.success("CORRECT");
+      CorrectAnswer();
+      sorted = true;
+    }
 
       for (let i = 0; i < mergedArray.length; i++) {
         blockItems.push([
