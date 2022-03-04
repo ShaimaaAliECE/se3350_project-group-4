a712bcc99040d3bebb9cd3ebd758313110310528
ncutler3 <ncutler3@uwo.ca>
2022-03-04 15:33:04 -0500

    pls work, changing default url to work on gcp

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -3,7 +3,7 @@ import _axios from "axios";
 // change the base url when sending axios requests
 const axios = (baseURL) => {
   const instance = _axios.create({
-    baseURL: baseURL || process.env.REACT_APP_API_DOMAIN ||"http://localhost:3001",
+    baseURL: baseURL || process.env.REACT_APP_API_DOMAIN ||`//${window.location.host}:3001/`,
     timeout: 1500,
   });
 
